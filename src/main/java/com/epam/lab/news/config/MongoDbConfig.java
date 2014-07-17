8a70f58d16ddffb3ad93f1cfeef8cb042ca21ea4
Dzmitry Piatrovich <piatrovich@live.ru>
2014-07-17 22:21:22 +0300

    Optimize imports

--- src/main/java/com/epam/lab/news/config/MongoDbConfig.java
+++ src/main/java/com/epam/lab/news/config/MongoDbConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
