7b770cb9a68be41fb7e7eb82b1496bd98fa18366
Dzmitry Piatrovich <piatrovich@live.ru>
2014-07-20 19:44:36 +0300

    Added aspects for observing connection pool. Enhanced connection pool. Added log4j. Configured default page for 404 error code

--- src/main/java/com/epam/lab/news/aop/logging/ApplicationAPILogger.java
+++ src/main/java/com/epam/lab/news/aop/logging/ApplicationAPILogger.java
@@ -1,15 +1,31 @@
 package com.epam.lab.news.aop.logging;
 
+import com.epam.lab.news.bean.Article;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 @Aspect
 public class ApplicationAPILogger {
+    private static Logger apiLogger = Logger.getLogger("api");
+    private static Logger errorLogger = Logger.getLogger("errors");
 
-    @AfterReturning(pointcut = "execution(* com.epam.lab.news.controller.ViewsController.*(..))",
-                    returning = "page")
-    public void listenReturnedPage(Object page) {
-        System.out.println(page);
+    @AfterReturning(pointcut = "execution(* com.epam.lab.news.controller.APIController.getArticleForView(..))",
+                    returning = "article")
+    public void listenReturnedPage(Article article) {
+        ObjectMapper mapper = new ObjectMapper();
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        mapper.setDateFormat(format);
+        try {
+            apiLogger.info(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(article));
+        } catch (JsonProcessingException e) {
+            errorLogger.error(e);
+        }
     }
 
 }
--- src/main/java/com/epam/lab/news/aop/observer/ConnectionPoolObserver.java
+++ src/main/java/com/epam/lab/news/aop/observer/ConnectionPoolObserver.java
@@ -1,35 +1,37 @@
 package com.epam.lab.news.aop.observer;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class ConnectionPoolObserver {
+    private static Logger logger = Logger.getLogger("pool");
 
     @Before(value = "@within(javax.annotation.PostConstruct)")       // first way
     public void observeStartInit(){
-        System.out.println("Starting init() method!");
+        logger.info("Starting init() method!");
     }
 
     @After(value = "@annotation(javax.annotation.PostConstruct)")   // another way
     //@Pointcut("call( public * com.epam.lab.news.database.jdbc.pool.ConnectionPool.size())")  It's not possible!
     public void observeFinishInit(){
-        System.out.println("Finishing init() method!");
+        logger.info("Finishing init() method!");
     }
 
     @AfterReturning(pointcut = "execution(* com.epam.lab.news.database.jdbc.pool.ConnectionPool.size())",
                     returning = "size")
     public void checkPoolSize(Integer size){
-        System.out.println("After init in pool " + size + " connections.");
+        logger.info("After init in pool " + size + " connections.");
     }
 
     @After("execution(* com.epam.lab.news.database.jdbc.pool.ConnectionPool.getConnection())")
     public void handleTakingConnection(){
-        System.out.println("Connection taken from pool.");
+        logger.info("Connection taken from pool.");
     }
 
     @After("execution(* com.epam.lab.news.database.jdbc.pool.ConnectionPool.returnConnection())")
     public void handleReturningConnection(){
-        System.out.println("Connection returned to pool.");
+        logger.info("Connection returned to pool.");
     }
 
 }
