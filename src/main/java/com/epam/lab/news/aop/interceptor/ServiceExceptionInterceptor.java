b6ced15882895d50978c751bb61b9561202540de
Dzmitry Piatrovich <piatrovich@live.ru>
2014-07-21 12:06:55 +0300

    Documented

--- src/main/java/com/epam/lab/news/aop/interceptor/ServiceExceptionInterceptor.java
+++ src/main/java/com/epam/lab/news/aop/interceptor/ServiceExceptionInterceptor.java
@@ -4,10 +4,21 @@ import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
+/**
+ * Handles exception which can be thrown from repositories
+ *
+ * @author Dzmitry Piatrovich
+ */
 @Aspect
 public class ServiceExceptionInterceptor {
+    /** Logger for errors */
     private static Logger logger = Logger.getLogger("errors");
 
+    /**
+     * Handles repositories exceptions
+     *
+     * @param exception If an error has occurred in repositories
+     */
     @AfterThrowing(pointcut = "execution(* com.epam.lab.news.database.data.service.NewsService.*(..))",
                    throwing = "exception")
     public void handleNewsServiceExceptions(Throwable exception){
--- src/main/java/com/epam/lab/news/aop/observer/ConnectionPoolObserver.java
+++ src/main/java/com/epam/lab/news/aop/observer/ConnectionPoolObserver.java
@@ -3,32 +3,55 @@ package com.epam.lab.news.aop.observer;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.*;
 
+/**
+ * Observes for custom pool activity
+ *
+ * @author Dzmitry Piatrovich
+ */
 @Aspect
 public class ConnectionPoolObserver {
+    /** Pool activity logger */
     private static Logger logger = Logger.getLogger("pool");
 
-    @Before(value = "@within(javax.annotation.PostConstruct)")       // first way
+    /**
+     * Watching start of initialization
+     */
+    @Before(value = "@within(javax.annotation.PostConstruct)")
     public void observeStartInit(){
         logger.info("Starting init() method!");
     }
 
-    @After(value = "@annotation(javax.annotation.PostConstruct)")   // another way
-    //@Pointcut("call( public * com.epam.lab.news.database.jdbc.pool.ConnectionPool.size())")  It's not possible!
+    /**
+     * Watching end of initialization
+     */
+    @After(value = "@annotation(javax.annotation.PostConstruct)")
+    //@Pointcut("call( public * com.epam.lab.news.database.jdbc.pool.ConnectionPool.size())")  Impossible!
     public void observeFinishInit(){
         logger.info("Finishing init() method!");
     }
 
+    /**
+     * Watching pool size
+     *
+     * @param size Number of connections
+     */
     @AfterReturning(pointcut = "execution(* com.epam.lab.news.database.jdbc.pool.ConnectionPool.size())",
                     returning = "size")
     public void checkPoolSize(Integer size){
         logger.info("After init in pool " + size + " connections.");
     }
 
+    /**
+     * Watching taking connections from pool
+     */
     @After("execution(* com.epam.lab.news.database.jdbc.pool.ConnectionPool.getConnection())")
     public void handleTakingConnection(){
         logger.info("Connection taken from pool.");
     }
 
+    /**
+     * Watching putting connections to pool
+     */
     @After("execution(* com.epam.lab.news.database.jdbc.pool.ConnectionPool.returnConnection())")
     public void handleReturningConnection(){
         logger.info("Connection returned to pool.");
