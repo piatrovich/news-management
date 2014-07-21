e6b48cfa490e99fbcf4552d45f4884d4b9f00ded
Dzmitry Piatrovich <piatrovich@live.ru>
2014-07-21 11:44:11 +0300

    Enhanced aspect which handles NewsService exceptions

--- src/main/java/com/epam/lab/news/aop/interceptor/ServiceExceptionInterceptor.java
+++ src/main/java/com/epam/lab/news/aop/interceptor/ServiceExceptionInterceptor.java
@@ -1,16 +1,17 @@
 package com.epam.lab.news.aop.interceptor;
 
-import org.aspectj.lang.JoinPoint;
+import org.apache.log4j.Logger;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class ServiceExceptionInterceptor {
+    private static Logger logger = Logger.getLogger("errors");
 
     @AfterThrowing(pointcut = "execution(* com.epam.lab.news.database.data.service.NewsService.*(..))",
                    throwing = "exception")
-    public void handleNewsServiceExceptions(JoinPoint point, Throwable exception){
-        System.out.println("ServiceExceptionInterceptor handle: " + exception.getMessage());
+    public void handleNewsServiceExceptions(Throwable exception){
+        logger.error("ServiceExceptionInterceptor handle: ", exception);
     }
 
 }
